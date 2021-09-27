4d40c481a8f2f7f7f1052920d10bfb209c717e26
KimrAndrew <andrewrk1997@gmail.com>
2021-09-27 16:25:01 -0700

    added assests folder

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let containerDiv = document.getElementById("product-selector");
+let leftProduct = document.getElementById("left-product");
+let rightProduct = document.getElementById("right-product");
+let centerProduct = document.getElementById("center-product");
+
 let productLineUp = [];
 
 let Product = function(name, path) {
@@ -15,13 +20,40 @@ let arrayContains = function(array, product) {
     return false;
 }
 
-let selectProductsToDisplay = function(numSelections) {
+let generateRandIndex = function() {
+    let selectionIndex = Math.floor(Math.random() * productLineUp.length);
+    return selectionIndex;
+}
+
+let selectProductsToDisplay = function() {
     let productsToDisplay = [];
-    for (let i = 0; i <= numSelections; i++) {
-        let selectionIndex = Math.floor(Math.random() * productLineUp.length);
+
+    //select given number of unique items from the product lineup
+    let productLineUpIndex = generateRandIndex();
+    for (let i = 0; i <= 3; i++) {
+        while (arrayContains(productsToDisplay, productLineUpIndex)) {
+            productLineUpIndex = generateRandIndex();
+        }
+        productsToDisplay.push(productLineUp[productLineUpIndex]);
     }
 
+    //increment times shown for each product selected
+    for (let i = 0; i < productsToDisplay.length; i++) {
+        productsToDisplay[i].timesShown++;
+    }
+
+    return productsToDisplay;
 }
+
+
+let renderProducts = function() {
+    let productsToDisplay = selectProductsToDisplay();
+    leftProduct.src = productsToDisplay[0];
+    centerProduct.src = productsToDisplay[1];
+    rightProduct.src = productsToDisplay[2];   
+}
+
+
 
 
 
