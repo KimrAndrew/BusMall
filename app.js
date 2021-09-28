753961b573c11f7d98d3a0a76881ccfbcaec3d7d
KimrAndrew <andrewrk1997@gmail.com>
2021-09-27 17:21:27 -0700

    random images now rendering

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ let leftProduct = document.getElementById("left-product");
 let rightProduct = document.getElementById("right-product");
 let centerProduct = document.getElementById("center-product");
 
+
 let productLineUp = [];
 
-let Product = function(name, path) {
+let Product = function(name, url) {
     this.name = name;
-    this.path = path;
+    this.url = "assets/" + url;
     this.timesShown = 0;
+    productLineUp.push(this);
 }
 
 let arrayContains = function(array, product) {
@@ -31,10 +33,11 @@ let selectProductsToDisplay = function() {
     //select given number of unique items from the product lineup
     let productLineUpIndex = generateRandIndex();
     for (let i = 0; i <= 3; i++) {
-        while (arrayContains(productsToDisplay, productLineUpIndex)) {
+        while (arrayContains(productsToDisplay, productLineUp[productLineUpIndex])) {
             productLineUpIndex = generateRandIndex();
         }
         productsToDisplay.push(productLineUp[productLineUpIndex]);
+        productLineUpIndex = generateRandIndex();
     }
 
     //increment times shown for each product selected
@@ -48,11 +51,37 @@ let selectProductsToDisplay = function() {
 
 let renderProducts = function() {
     let productsToDisplay = selectProductsToDisplay();
-    leftProduct.src = productsToDisplay[0];
-    centerProduct.src = productsToDisplay[1];
-    rightProduct.src = productsToDisplay[2];   
+    console.log(productsToDisplay[0].name, productsToDisplay[1].name, productsToDisplay[2].name);
+    leftProduct.src = productsToDisplay[0].url;
+    centerProduct.src = productsToDisplay[1].url;
+    rightProduct.src = productsToDisplay[2].url;
+    console.log(rightProduct.src);
+    console.log(typeof leftProduct.src);
+    console.log("done");  
 }
 
+let Bag = new Product("bag", "bag.jpeg");
+let Banana = new Product("banana", "banana.jpeg");
+let Bathroom = new Product("bathroom","bathroom.jpeg");
+let Boots = new Product("boots", "boots.jpeg");
+let Breakfast = new Product("breakfast", "breakfast.jpeg");
+let Bubblegum = new Product("bubblegum","bubblegum.jpeg");
+let Chair = new Product("chair", "bubblegum.jpeg");
+let Cthulhu = new Product("cthulhu", "cthulhu.jpeg");
+let DogDuck = new Product("dog-duck", "dog-duck.jpeg");
+let Dragon = new Product("dragon", "dragon.jpeg");
+let Pen = new Product("pen","pen.jpeg");
+let PetSweep = new Product("pet-sweep", "pet-sweep.jpeg");
+let Scissors = new Product("scissors", "scissors.jpeg");
+let Shark = new Product("shark", "shark.jpeg");
+let Sweep = new Product("sweep", "sweep.png");
+let TaunTaun = new Product("tauntaun", "tauntaun.jpeg");
+let Unicorn = new Product("unicorn", "unicorn.jpeg");
+let WaterCan = new Product("water-can", "water-can.jpeg");
+let WineGlass = new Product("wine-glass", "wine-glass.jpeg");
+
+leftProduct.src = Bag.url;
+renderProducts();
 
 
 
